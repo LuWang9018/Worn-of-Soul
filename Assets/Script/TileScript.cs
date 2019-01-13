b1942236ad5d36d2c99cfdee9e9e91fe5558e677
lu <Canada_wl@aliyun.com>
2019-01-12 19:23:10 -0500

    init

---
+++ Assets/Script/TileScript.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour {
+
+    public bool walkable = true;
+    public bool current = false;
+    public bool target = false;
+    public bool selectable = false;
+
+    public List<TileScript> adjacencyList = new List<TileScript>();
+
+    public bool visited = false;
+    public TileScript parent = null;
+    public int distance = 0;
+
+    public float f = 0;
+    public float g = 0;
+    public float h = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (current)
+        {
+            GetComponent<Renderer>().material.color = Color.magenta;
+        }
+        else if (target)
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        else if (selectable)
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        else
+        {
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
+
+    public void Reset()
+    {
+        adjacencyList.Clear();
+
+        current = false;
+        target = false;
+        selectable = false;
+
+        visited = false;
+        parent = null;
+        distance = 0;
+    }
+
+    public void FindNeighbors(float jumpHeight, TileScript target)
+    {
+        Reset();
+
+        CheckTile(Vector3.forward, jumpHeight, target);
+        CheckTile(-Vector3.forward, jumpHeight, target);
+        CheckTile(Vector3.right, jumpHeight, target);
+        CheckTile(-Vector3.right, jumpHeight, target);
+    }
+
+    public void CheckTile(Vector3 direction, float jumpHeight, TileScript target)
+    {
+        Vector3 halfExtents = new Vector3(0.25f, (2 + jumpHeight) / 2.0f, 0.25f);
+        Collider[] colliders = Physics.OverlapBox(transform.position + direction, halfExtents);
+
+        foreach(Collider item in colliders)
+        {
+            TileScript tile = item.GetComponent<TileScript>();
+            if(tile != null && tile.walkable)
+            {
+                RaycastHit hit;
+                if(!Physics.Raycast(tile.transform.position,Vector3.up, out hit, 1)||(tile==target))
+                {
+                    adjacencyList.Add(tile);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Script/TurnManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour 
+{
+    static Dictionary<string, List<TacticMove>> units = new Dictionary<string, List<TacticMove>>();
+    static Queue<string> turnKey = new Queue<string>();
+    static Queue<TacticMove> turnTeam = new Queue<TacticMove>();
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        if (turnTeam.Count == 0)
+        {
+            InitTeamTurnQueue();
+        }
+	}
+
+    static void InitTeamTurnQueue()
+    {
+        List<TacticMove> teamList = units[turnKey.Peek()];
+
+        foreach (TacticMove unit in teamList)
+        {
+            turnTeam.Enqueue(unit);
+        }
+
+        StartTurn();
+    }
+
+    public static void StartTurn()
+    {
+        if (turnTeam.Count > 0)
+        {
+            turnTeam.Peek().BeginTurn();
+        }
+    }
+
+    public static void EndTurn()
+    {
+        TacticMove unit = turnTeam.Dequeue();
+        unit.EndTurn();
+
+        if (turnTeam.Count > 0)
+        {
+            StartTurn();
+        }
+        else
+        {
+            string team = turnKey.Dequeue();
+            turnKey.Enqueue(team);
+            InitTeamTurnQueue();
+        }
+    }
+
+    public static void AddUnit(TacticMove unit)
+    {
+        List<TacticMove> list;
+
+        if (!units.ContainsKey(unit.tag))
+        {
+            list = new List<TacticMove>();
+            units[unit.tag] = list;
+
+            if (!turnKey.Contains(unit.tag))
+            {
+                turnKey.Enqueue(unit.tag);
+            }
+        }
+        else
+        {
+            list = units[unit.tag];
+        }
+
+        list.Add(unit);
+    }
+}
